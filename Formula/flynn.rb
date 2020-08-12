e6b1278016f704dd7482cefd0d0c38149e94021a
Philipp Haußleiter <philipp@haussleiter.de>
2020-08-12 21:51:29 +0200

    updating flynn to v20200804.1-1ecd12c

--- Formula/flynn.rb
+++ Formula/flynn.rb
@@ -1,11 +1,11 @@
 class Flynn < Formula
-  $flynn_hash = "ff3251a66acdafcf5eab0d75daa7a059532810f7f5f38c55cf33ab61528189a61e0b268903b8f5746a2ac84c9ba21b013ad14a70ac53c66d5a8241f3cb28f90e"
+  $flynn_hash = "dc429a95b99d2f75adc906275d317b25a2a6ce09886ea101473e2c4b1cb44426e5ae104db943e2f1a46a165c0f7dd086c2954f1a7b274910b669989651340f3e"
 
   desc "Everything you need to get started with Flynn"
   homepage "https://flynn.io/docs/cli"
   url "https://dl.flynn.io/tuf/targets/#{$flynn_hash}.flynn-darwin-amd64.gz"
-  version "v20200329.0-755c956"
-  sha256 "faa483efed92e241e5c70518b9d82cdfdbf247841d2bbf56592cb2ff8c7c3367"
+  version "v20200804.1-1ecd12c"
+  sha256 "c7906be3ba637ef4c4bfdef4a7964e17e0f27c854fca6ff2351033ba5f2aab2f"
 
   def install
     system "mkdir", "bin"
