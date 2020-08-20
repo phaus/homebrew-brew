aa1eb834f750e736bc1a047eda23222d7126ee81
Philipp Haußleiter <philipp@haussleiter.de>
2020-08-20 11:13:55 +0200

    fixing flynn checksum

--- Formula/flynn.rb
+++ Formula/flynn.rb
@@ -5,7 +5,7 @@ class Flynn < Formula
   homepage "https://flynn.io/docs/cli"
   url "https://dl.flynn.io/tuf/targets/#{$flynn_hash}.flynn-darwin-amd64.gz"
   version "v20200818.0-eecb13b"
-  sha256 "4ca5b5af9bd124be47b29e187f060487e0633012857f34df1bc712e0da07ce05"
+  sha256 "b1f775022e850da953d88a6058415068cba2c91a8fb09ba135ed1332ecc07f36"
 
   def install
     system "mkdir", "bin"
